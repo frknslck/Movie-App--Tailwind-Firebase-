657855eb07e45da5ad525085a9470e1b672a0dd7
frknslck <furkanselcuk.fs58@gmail.com>
2023-03-14 20:07:34 +0300

    card hover

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const MovieCard = ({movie}) => {
     const { poster_path, title, overview,vote_average } = movie
@@ -8,13 +8,17 @@ const MovieCard = ({movie}) => {
         <>
           <div className="flex justify-center">
             <div className="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700">
-              <a href="#!" data-te-ripple-init="" data-te-ripple-color="light">
+              <div className='relative group' data-te-ripple-init="" data-te-ripple-color="light">
                 <img
                   className="rounded-t-lg"
                   src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
                   alt=""
                 />
-              </a>
+                <div className="absolute h-full w-full bg-black/90 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300 text-white flex-col px-5 gap-5">
+                  <h3 className='text-3xl'>Overview</h3>
+                  <p>{overview}</p>
+                </div>
+              </div>
               <div className=" p-6">
                 <div className='flex justify-between gap-5 h-[60px]'>
                   <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
@@ -26,12 +30,11 @@ const MovieCard = ({movie}) => {
                 </div>
                 <button
                   type="button"
-                  className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                  class="inline-block rounded bg-danger px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)]"
                   data-te-ripple-init=""
                   data-te-ripple-color="light"
-                  onClick={() => navigate(`/details/${movie.id}`)}
-                >
-                  See Details
+                  onClick={() => navigate(`/details/${movie.id}`)}>
+                  More Details
                 </button>
               </div>
             </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -53,7 +53,7 @@ const Main = ({movies, setMovies}) => {
             onChange={(e) => setSearch(e.target.value)}/>
           <button
             type="submit"
-            className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            className="text-white absolute right-2.5 bottom-2.5 bg-danger hover:bg-danger-600 focus:ring-4 focus:outline-none focus:bg-danger-600 font-medium rounded-lg text-sm px-4 py-2">
             Search
           </button>
         </div>
