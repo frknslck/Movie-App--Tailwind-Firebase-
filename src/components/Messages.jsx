24dc57ca1d6a2b52b1e3eefbf61fcaee1df8d5ee
frknslck <furkanselcuk.fs58@gmail.com>
2023-03-18 12:42:57 +0300

    time of messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {auth} from '../auth/firebase'
 
 const Message = ({ message }) => {
-
+  const{name, photoURL, timestamp, uid, text} = message
   const messageClass = (uid) => {
     if (uid === auth.currentUser.uid) {
       return "bg-red-700 dark:text-white flex-row-reverse text-end ml-auto rounded-bl-full";
@@ -10,15 +10,16 @@ const Message = ({ message }) => {
       return "bg-[#e5e5ea] text-black mr-auto rounded-br-full";
     }
   };
-
+  let hour = (new Date(timestamp.seconds*1000).getHours().toString().padStart(2, '0'))
+  let min = (new Date(timestamp.seconds*1000).getMinutes().toString().padStart(2, '0'))
   return (
     <div>
-      <div className={`flex w-fit items-center shadow-xl my-10 py-2 px-3 rounded-tl-full rounded-tr-full ${messageClass(message.uid)}`}> 
+      <div className={`flex w-fit items-center shadow-xl my-10 py-2 px-3 rounded-tl-full rounded-tr-full ${messageClass(uid)}`}> 
         <div className='flex gap-2 absolute mt-[-4.5rem] '>
-            <p className=" dark:text-white text-xs">{message.name}</p>
-            <img className='w-6 h-6 rounded-full' src={message.photoURL} alt="" />
+            <p className=" dark:text-white text-xs">{name}</p>
+            <img className='w-6 h-6 rounded-full' src={photoURL} alt="" />
         </div>
-        <p className=''>{message.text}</p>
+        <p>{text}<span className='pl-3 text-[0.75rem]'>{`${hour}:${min}`}</span></p>
       </div>
     </div>
   );
